3a2e894b4c5983503a3dc1a95adff6d66fa37837
joao paulo <jpcamargo+github@gmail.com>
2010-05-21 17:40:39 -0300

    changed/added a time_config constant with hour configurations

--- entry.rb
+++ entry.rb
@@ -65,10 +65,12 @@ class Entry < ActiveRecord::Base
     if !rows.empty?
       rows.each {|row| eval("#{row.status} = row")}
       stop = time if stop.nil?
-      lunch_duration = (!back.nil? and !lunch.nil?) ? back.to_i - lunch.to_i : (!lunch.nil?) ? time.to_i - lunch.to_i : 0
-      req_lunch_duration = (lunch_duration >= MINIMUM_LUNCH_DURATION) ? lunch_duration : MINIMUM_LUNCH_DURATION
-      lunch_duration = Time.at(lunch_duration + FIX_TIME)
-      total = Time.at(stop.to_i - start.to_i - (((stop.to_i - start.to_i >= 21600) or (!lunch.nil? and !back.nil?)) ? req_lunch_duration : 0) + FIX_TIME)
+      total_ut = stop.to_i - start.to_i
+      minimum_lunch_duration = (total_ut <= 21600) ? TIME_CONFIG[:lunch][:upto6] : TIME_CONFIG[:lunch][:full]
+      lunch_duration = (!back.nil? && !lunch.nil?) ? back.to_i - lunch.to_i : (!lunch.nil?) ? time.to_i - lunch.to_i : 0
+      req_lunch_duration = (lunch_duration >= minimum_lunch_duration) ? lunch_duration : minimum_lunch_duration
+      lunch_duration = Time.at(lunch_duration + TIME_CONFIG[:fix_time])
+      total = Time.at(stop.to_i - start.to_i - (((total_ut > 21600) || (!lunch.nil? && !back.nil?)) ? req_lunch_duration : 0) + TIME_CONFIG[:fix_time])
     end
     return [start, lunch, back, stop, lunch_duration, total]
   end
