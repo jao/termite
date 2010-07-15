158393d3d3abd77f09bf066b7e5c0f00b7397413
joao paulo <jpcamargo+github@gmail.com>
2010-07-15 13:48:05 -0300

    added regional holidays for all brazilian states

--- lib/locale.pt-br.rb
+++ lib/locale.pt-br.rb
@@ -37,8 +37,111 @@ other_holidays << {:date => (eastern_date.days(-47)).day_month, :label => "Carna
 other_holidays << {:date => (eastern_date.days(60)).day_month, :label => "Corpus Christi"}
 HOLIDAYS.concat(other_holidays)
 
-# sao paulo specific holidays
-sp_br = [
-  {:date => '09/07', :label => 'Nove de Julho'}
-]
-HOLIDAYS.concat(sp_br)
+# state/region specific holidays
+REGIONAL_HOLIDAYS = {
+  :AC => [
+    {:date => '23/01', :label => 'Dia do evangelico'},
+    {:date => '15/06', :label => 'Aniversario do estado'},
+    {:date => '06/08', :label => 'Inicio da Revolucao Acreana'},
+    {:date => '05/09', :label => 'Dia da Amazonia'},
+    {:date => '17/11', :label => 'Assinatura do Tratado de Petropolis'}
+  ],
+  :AL => [
+    {:date => '24/06', :label => 'Sao Joao'},
+    {:date => '29/06', :label => 'Sao Pedro'},
+    {:date => '16/09', :label => 'Emancipacao politica'},
+    {:date => '20/11', :label => 'Dia da consciencia Negra'}
+  ],
+  :AP => [
+    {:date => '19/03', :label => 'Dia de Sao Jose'},
+    {:date => '05/10', :label => 'Criacao do estado'},
+    {:date => '20/11', :label => 'Dia da consciencia Negra'}
+  ],
+  :AM => [
+    {:date => '05/09', :label => 'Elevacao do Amazonas a categoria de provincia'},
+    {:date => '20/11', :label => 'Dia da consciencia Negra'},
+    {:date => '08/12', :label => 'Dia de Nossa senhora da Conceicao'}
+  ],
+  :BA => [
+    {:date => '28/06', :label => 'Piloes'},
+    {:date => '02/07', :label => 'Independencia da Bahia'},
+    {:date => '20/11', :label => 'Dia da consciencia Negra'}
+  ],
+  :DF => [
+    {:date => '21/04', :label => 'Fundacao de Brasilia'},
+    {:date => '30/11', :label => 'Dia do evangelico'}
+  ],
+  :ES => [
+    {:date => '23/05', :label => 'Colonizacao do solo espirito-santense'},
+    {:date => '28/10', :label => 'Dia do servidor publico'}
+  ],
+  :GO => [
+    {:date => '28/10', :label => 'Dia do servidor publico'}
+  ],
+  :MA => [
+    {:date => '28/07', :label => 'Adesao do Maranhao a Independencia do Brasil'},
+    {:date => '08/12', :label => 'Dia de Nossa Senhora Conceicao'}
+  ],
+  :MT => [
+    {:date => '20/11', :label => 'Dia da Consciencia Negra'}
+  ],
+  :MS => [
+    {:date => '11/10', :label => 'Criacao do estado'}
+  ],
+  :MG => [
+    {:date => '21/04', :label => 'Tiradentes'}
+  ],
+  :PA => [
+    {:date => '15/08', :label => 'Adesao do Grao-Para a Independencia do Brasil'},
+    {:date => '08/12', :label => 'Dia de Nossa Senhora Conceicao'}
+  ],
+  :PB => [
+    {:date => '05/08', :label => 'Fundacao do Estado em 1585'}
+  ],
+  :PR => [
+    {:date => '19/12', :label => 'Emancipacao politica'}
+  ],
+  :PE => [
+    {:date => '06/03', :label => 'Revolucao Pernambucana de 1817'},
+    {:date => '24/06', :label => 'Sao Joao'}
+  ],
+  :PI => [
+    {:date => '13/03', :label => 'Dia da Batalha do Jenipapo'},
+    {:date => '19/10', :label => 'Dia do Piaui'}
+  ],
+  :RJ => [
+    {:date => '20/01', :label => 'Dia de Sao Sebastiao'},
+    {:date => '23/04', :label => 'Dia de Sao Jorge'},
+    {:date => '15/10', :label => 'Dia do comercio'},
+    {:date => '28/10', :label => 'Dia do funcionario publico'},
+    {:date => '20/11', :label => 'Dia da Consciencia Negra'}
+  ],
+  :RN => [
+    {:date => '29/06', :label => 'Dia de Sao Pedro'},
+    {:date => '03/10', :label => 'Martires de Cunhau e Uruacuu'}
+  ],
+  :RS => [
+    {:date => '20/09', :label => 'Revolucao Farroupilha'}
+  ],
+  :RO => [
+    {:date => '04/01', :label => 'Criacao do estado'},
+    {:date => '18/06', :label => 'Dia do evangelico'}
+  ],
+  :RR => [
+    {:date => '05/10', :label => 'Criacao do estado'}
+  ],
+  :SC => [
+    {:date => '11/08', :label => 'Criacao da capitania, separando-se de Sao Paulo'}
+  ],
+  :SP => [
+    {:date => '09/07', :label => 'Revolucao constitucionalista de 1932'},
+    {:date => '20/11', :label => 'Dia da Consciencia Negra'}
+  ],
+  :SE => [
+    {:date => '08/07', :label => 'Autonomia politica de Sergipe'}
+  ],
+  :TO => [
+    {:date => '05/10', :label => 'Criacao do estado'}
+  ],
+}
+HOLIDAYS.concat(REGIONAL_HOLIDAYS[REGION.to_sym])
