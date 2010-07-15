345f80cfd3cc892a3e76b047f14c25262dabd773
joao paulo <jpcamargo+github@gmail.com>
2010-07-14 21:17:48 -0300

    moved workday time configuration to the brazilian locale, including a holidays hash - needs to accept rules to auto generate some non-fixed date events

---
+++ lib/locale.pt-br.rb
@@ -0,0 +1,23 @@
+TIME_CONFIG = {
+  :start => 9,
+  :end => 18,
+  :total => 8,
+  :lunch => {:upto6 => 900, :full => 3600},
+  :fix_time => 10800
+}
+
+HOLIDAYS = {
+  '01/01/2010' => 'Confraternização Universal',
+  '15/02/2010' => 'Carnaval',
+  '16/02/2010' => 'Carnaval',
+  '02/04/2010' => 'Paixão de Cristo',
+  '21/04/2010' => 'Tiradentes',
+  '01/05/2010' => 'Dia do Trabalho',
+  '03/06/2010' => 'Corpus Christi',
+  '09/07/2010' => 'Nove de Julho',
+  '07/09/2010' => 'Independência do Brasil',
+  '12/10/2010' => 'Nossa Sra. Aparecida - Padroeira do Brasil',
+  '02/11/2010' => 'Finados',
+  '15/11/2010' => 'Proclamação da República',
+  '25/12/2010' => 'Natal'
+}
